eb8fc2a71d2a2d14bb0eb6900ab29d5f3d1d63f1
longdoan0311@gmail.com <longdt123>
2019-10-28 22:30:28 +0700

    add security

--- src/main/java/com/viettel/imdb/rest/config/Config.java
+++ src/main/java/com/viettel/imdb/rest/config/Config.java
@@ -21,9 +21,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.ArrayList;
 import java.util.List;
